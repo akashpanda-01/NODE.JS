c83bea0b1503b91e8bad4a091cc60f2f949df9fb
akashpanda-01 <1999akashpanda@gmail.com>
2026-07-05 11:37:36 +0530

    Practicing Questions

--- practice.js
+++ practice.js
@@ -100,3 +100,25 @@ function vowelsAndConsonants(s) {
 }
 // vowelsAndConsonants("javAScriptLoop");
 
+function regexVar(s) {
+  let re = /^([aeiou]).*\1$/;
+  return re;
+}
+// const val = regexVar();
+// console.log(val.test("abcda"));
+
+
+// Complete the reverseString function; it has one parameter, . You must perform the following actions:
+// Try to reverse string  using the split, reverse, and join methods.
+// If an exception is thrown, catch it and print the contents of the exception's  on a new line.
+// Print  on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
+function reverseString(s) {
+    try{
+      console.log(s.split("").reverse().join(""));
+    } catch(err){
+      console.log(err.message);
+      console.log(s);
+    };
+}
+reverseString("akash")
