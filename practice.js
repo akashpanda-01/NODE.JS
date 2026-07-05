a20479149400b46c839be480d6f91c97944a0df8
akashpanda-01 <1999akashpanda@gmail.com>
2026-07-05 11:32:51 +0530

    Date-5 Practicing Questions

---
+++ practice.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// process.stdin.resume();
+// process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+// process.stdin.on("data", (inputStdin) => {
+//   inputString += inputStdin;
+// });
+
+// process.stdin.on("end", (_) => {
+//   inputString = inputStdin
+//     .trim()
+//     .split("\n")
+//     .map((string) => {
+//       return string.trim();
+//     });
+//   main();
+// });
+
+// function readline() {
+//   return inputString[currentLine++];
+// }
+
+// function greeting(parameterValue) {
+//   console.log(parameterValue);
+
+//   console.log("Hello, World!");
+//   console.log("Welcome to 10 Days of JavaScript!");
+// }
+
+// function main() {
+//   const parameterValue = readline();
+//   greeting(parameterVariable);
+// }
+
