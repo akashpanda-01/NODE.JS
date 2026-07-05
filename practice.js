170f832956eff5142a3dd206188ff60241b39286
akashpanda-01 <1999akashpanda@gmail.com>
2026-07-05 11:34:23 +0530

    Practicing Questions

--- practice.js
+++ practice.js
@@ -36,3 +36,49 @@ let currentLine = 0;
 //   greeting(parameterVariable);
 // }
 
+// Convert  to an integer (Number type), then sum it with  and print the result on a new line using console.log.
+// Convert  to a floating-point number (Number type), then sum it with  and print the result on a new line using console.log.
+// Print the concatenation of  and  on a new line using console.log. Note that  must be printed first.
+// 'use strict';
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
+
+// let inputString = '';
+// let currentLine = 0;
+
+// process.stdin.on('data', inputStdin => {
+//     inputString += inputStdin;
+// });
+
+// process.stdin.on('end', _ => {
+//     inputString = inputString.trim().split('\n').map(string => {
+//         return string.trim();
+//     });
+
+//     main();
+// });
+
+// function readLine() {
+//     return inputString[currentLine++];
+// }
+
+// function performOperation(secondInteger, secondDecimal, secondString) {
+//     const firstInteger = 4;
+
+//     const firstDecimal = 4.0;
+
+//     const firstString = 'HackerRank ';
+//     console.log(firstInteger + Number(secondInteger));
+//     console.log(firstDecimal + Number(secondDecimal));
+//     console.log(firstString + secondString);
+// }
+
+// function main() {
+//     const secondInteger = readLine();
+//     const secondDecimal = readLine();
+//     const secondString = readLine();
+
+//     performOperation(secondInteger, secondDecimal, secondString);
+// }
+
