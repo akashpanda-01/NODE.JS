6dd1aa5255f3452b6d229c8b398643d797572c9d
akashpanda-01 <1999akashpanda@gmail.com>
2026-07-09 10:15:18 +0530

    Date-9 Database Collection Methods

--- database.js
+++ database.js
@@ -1,15 +1,15 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const url =
   "mongodb+srv://akash_db:zsA7KyhcGmyV54Q0@nodelearningdev.6wdrxxd.mongodb.net/";
-  const client = new MongoClient(url);
+const client = new MongoClient(url);
 
 const dbName = "NamasteNode";
 
 const data = {
-  "firstname" : "A",
-  "lastname": "B",
-  "city": "Mumbai"
+  firstname: "A",
+  lastname: "B",
+  city: "Mumbai",
 };
 
 async function main() {
@@ -18,12 +18,13 @@ async function main() {
   const db = client.db(dbName);
   const collection = db.collection("User");
 
+  // const createIndex = await collection.createIndex({firstname: 1});
   // const insterData = await collection.insertMany([data]);
+  // const removeData = await collection.deleteOne({
+  //   _id: new ObjectId("6a4f257ddae8c5fe0b4c6311"),
+  // });
   const findResult = await collection.find({}).toArray();
-  const removeData = await collection.deleteOne({_id : '6a4e6b5bdae1d77ebcb20bda'})
   console.log(findResult);
-  // console.log(insterData);
-  
 
   return "Done";
 }
