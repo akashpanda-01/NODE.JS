d824121c1b2e837f902c4a5ce99534927d576ce8
akashpanda-01 <1999akashpanda@gmail.com>
2026-07-15 10:38:21 +0530

    Date-15

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
-const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 const url =
   "mongodb+srv://akash_db:zsA7KyhcGmyV54Q0@nodelearningdev.6wdrxxd.mongodb.net/";
+
 const client = new MongoClient(url);
 
 const dbName = "NamasteNode";
@@ -9,22 +10,43 @@ const dbName = "NamasteNode";
 const data = {
   firstname: "A",
   lastname: "B",
-  city: "Mumbai",
+  city: "c",
 };
 
 async function main() {
   await client.connect();
-  console.log("Connected Successfully To Server");
+  console.log("Connected");
   const db = client.db(dbName);
   const collection = db.collection("User");
 
-  // const createIndex = await collection.createIndex({firstname: 1});
-  // const insterData = await collection.insertMany([data]);
-  // const removeData = await collection.deleteOne({
-  //   _id: new ObjectId("6a4f257ddae8c5fe0b4c6311"),
+  // CURD
+  // const create = await collection.insertOne(data);
+  // console.log(create);
+
+  const update = await collection.updateMany(
+    { city: "c" },
+    { $set: { city: "d" } },
+  );
+
+  // If multiple same document in database and we want to update specific one, so we
+  //have to find all the same documents and which one we want to change we have to use that
+  //specific ones id and update it like below code
+  // const data1 = await collection
+  //   .find({ firstname: "A", lastname: "B", city: "c" })
+  //   .toArray();
+  // const change = await collection.updateOne(
+  //   { _id: data1[1]._id },
+  //   { $set: { city: "d" } },
+  // );
+
+
+  // const remove = await collection.deleteOne({
+  //   firstname: "A",
   // });
-  const findResult = await collection.find({}).toArray();
-  console.log(findResult);
+
+  const find = await collection.find({}).toArray();
+  console.log(find);
+  // console.log(remove);
 
   return "Done";
 }
--- practice.js
+++ practice.js
@@ -123,48 +123,48 @@ function reverseString(s) {
 }
 // reverseString("akash")
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let currentLine = 0;
+// let inputString = '';
+// let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
+// process.stdin.on('data', inputStdin => {
+//     inputString += inputStdin;
+// });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
+// process.stdin.on('end', _ => {
+//     inputString = inputString.trim().split('\n').map(string => {
+//         return string.trim();
+//     });
     
-    main();    
-});
+//     main();    
+// });
 
-function readLine() {
-    return inputString[currentLine++];
-}
+// function readLine() {
+//     return inputString[currentLine++];
+// }
 
-function isPositive(a) {
-    if(a > 0){
-        return "YES";
-    } else if(a == 0){
-        throw new Error("Zero Error");
-    } else{
-        throw new Error("Negative Error");
-    }
-}
+// function isPositive(a) {
+//     if(a > 0){
+//         return "YES";
+//     } else if(a == 0){
+//         throw new Error("Zero Error");
+//     } else{
+//         throw new Error("Negative Error");
+//     }
+// }
 
-function main() {
-    const n = +(readLine());
+// function main() {
+//     const n = +(readLine());
     
-    for (let i = 0; i < n; i++) {
-        const a = +(readLine());
+//     for (let i = 0; i < n; i++) {
+//         const a = +(readLine());
       
-        try {
-            console.log(isPositive(a));
-        } catch (e) {
-            console.log(e.message);
-        }
-    }
-}
+//         try {
+//             console.log(isPositive(a));
+//         } catch (e) {
+//             console.log(e.message);
+//         }
+//     }
+// }
