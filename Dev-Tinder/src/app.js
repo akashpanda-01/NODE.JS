f1adb942dc82b4649291c819f0010f1d9ee1ce11
akashpanda-01 <1999akashpanda@gmail.com>
2026-07-22 11:33:49 +0530

    Date-22 Handling Routes, HTTP MRTHODS

--- Dev-Tinder/src/app.js
+++ Dev-Tinder/src/app.js
@@ -2,26 +2,39 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello/2", (req, res) => {
-    res.send("Hello URL 2");
-});
+// HTTP METHODS
+// app.get("/user", (req, res) => {
+//     res.send("User URL");
+// });
 
-app.use("/hello", (req, res) => {
-    res.send("Hello Url");
-});
+// app.post("/user", (req, res) => {
+//     res.send({Fname: "A", Lname: "b"});
+// });
 
-app.use("/test/1", (req, res) => {
-    res.send("Test URL 1");
-});
+// app.delete("/user", (req, res) => {
+//     res.send("Deleted");
+// });
 
-app.use("/test", (req, res) => {
-    res.send("Test Router Url");
-});
+// app.use("/hello/2", (req, res) => {
+//     res.send("Hello URL 2");
+// });
+
+// app.use("/hello", (req, res) => {
+//     res.send("Hello Url");
+// });
+
+// app.use("/test/1", (req, res) => {
+//     res.send("Test URL 1");
+// });
+
+// app.use("/test", (req, res) => {
+//     res.send("Test Router Url");
+// });
 
 
-app.use("/", (req, res) => {
-    res.send("Hello World");
-});
+// app.use("/", (req, res) => {
+//     res.send("Hello World");
+// });
 
 app.listen(3000, () => {
     console.log("Server Is Listening....");
