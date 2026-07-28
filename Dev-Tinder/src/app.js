0ef5966140a213451a5ef8ef016be0d83173fab0
akashpanda-01 <1999akashpanda@gmail.com>
2026-07-28 10:36:34 +0530

    Date-28, Route Handling

--- Dev-Tinder/src/app.js
+++ Dev-Tinder/src/app.js
@@ -2,45 +2,107 @@ const express = require("express");
 
 const app = express();
 
-// Dynamic API's Handling
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.end("Dynamic Data");
+// Route Handling
+app.use("/user", (req, res, next) => {
+    console.log("User 1 Fetched Successfully");
+    res.send("Hello User");
+    next();
+},(req, res) => {
+    console.log("User 2")
+    res.send("Hello User 2");
 });
 
-// Getting Query Params
-app.get("/user", (req, res) => {
-    console.log(req.query)
-    res.send({Fname: "A", Lname: "B"});
-});
+// Using Next Function Before res.send()
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("User 1 Fetched Successfully");
+    next();
+    res.send("Hello User 1");
+  },
+  (req, res) => {
+    console.log("User 2");
+    res.send("Hello User 2");
+  });
 
-//ADVANCED ROUTING CONCEPTS
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("User 1 Fetched Successfully");
+    // res.send("Hello User");
+    next();
+  },
+  (req, res, next) => {
+    console.log("User 2");
+    res.send("Hello User 2");
+  },
+  (req, res, next) => {
+    console.log("User 3");
+    res.send("Hello User 2");
+  },
+  (req, res, next) => {
+    console.log("User 4");
+    res.send("Hello User 2");
+  },
+  (req, res, next) => {
+    console.log("User 5");
+    res.send("Hello User 2");
+  },
+);
 
-app.get("/abc", (req, res) => {
-    res.send("abc Testing");
-});
+// We Can Add as many Route Handler Functions Inside An Array
+app.use("/user", (req, res, next) => {
+    next();
+    res.send("Hello User");
+}, [
+    (req, res) => {
+        console.log("Handler 1 In Array");
+    },
+    (req, res) => {
+        console.log("Handler 2 in Array");
+    }
+])
 
-// Optional (?) Method
-app.get(/^\/ab?c$/, (req, res) => {
-    res.send("abc ? Testing");
-});
+// // Dynamic API's Handling
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//     console.log(req.params);
+// );
 
-app.get(/^\/a(bc)?d$/, (req, res) => {
-    res.send("abc ? Testing");
-});
+//     res.end("Dynamic Data");
+// });
 
-//(+) MRTHOD
-app.get(/^\/ab+c$/, (req, res) => {
-    res.send("abc +")
-});
+// // Getting Query Params
+// app.get("/user", (req, res) => {
+//     console.log(req.query)
+//     res.send({Fname: "A", Lname: "B"});
+// });
 
-app.get(/^\/a(bc)+d$/, (req, res) => {
-    res.send("abc +")
-});
+// //ADVANCED ROUTING CONCEPTS
 
+// app.get("/abc", (req, res) => {
+//     res.send("abc Testing");
+// });
+
+// // Optional (?) Method
+// app.get(/^\/ab?c$/, (req, res) => {
+//     res.send("abc ? Testing");
+// });
+
+// app.get(/^\/a(bc)?d$/, (req, res) => {
+//     res.send("abc ? Testing");
+// });
+
+// //(+) METHOD
+// app.get(/^\/ab+c$/, (req, res) => {
+//     res.send("abc +")
+// });
+
+// app.get(/^\/a(bc)+d$/, (req, res) => {
+//     res.send("abcd +")
+// });
 
 // app.get(/.*fly$/, (req, res) => {
-//     res.send("Worked");
+//     res.send("Worked fly");
 // });
 
 // HTTP METHODS
@@ -72,11 +134,10 @@ app.get(/^\/a(bc)+d$/, (req, res) => {
 //     res.send("Test Router Url");
 // });
 
-
 // app.use("/", (req, res) => {
 //     res.send("Hello World");
 // });
 
 app.listen(3000, () => {
-    console.log("Server Is Listening....");
+  console.log("Server Is Listening....");
 });
