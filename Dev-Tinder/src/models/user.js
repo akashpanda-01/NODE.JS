10030000fb360b41f52f3d8c041b86829a72e1c3
akashpanda-01 <1999akashpanda@gmail.com>
2026-08-10 20:10:18 +0530

    Date-10

--- Dev-Tinder/src/models/user.js
+++ Dev-Tinder/src/models/user.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: String },
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 25,
+  },
   lastName: { type: String },
   age: { type: Number },
+  emailId: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+    trim: true,
+  },
+
+  password: { type: Number, required: true },
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender Data Is Not Valid");
+      }
+    },
+  },
 });
 
 const User = mongoose.model("User", userSchema);
--- Dev-Tinder/src/server.js
+++ Dev-Tinder/src/server.js
@@ -30,52 +30,52 @@ server.get("/user", async (req, res) => {
     res.send(user);
   } catch (error) {
     res.status(400).send("Something Went Wrong..");
-  };
-  
+  }
 });
 
 // Get All The Users or documents from our Database
 server.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
-    if(users.length === 0){
+    if (users.length === 0) {
       res.send("Users Not Found");
     } else {
       res.send(users);
     }
   } catch (error) {
-    res.status(400).send("Something Went Wrong")
-  };
+    res.status(400).send("Something Went Wrong");
+  }
 });
-
 
 // Delete a User Document
 server.delete("/deleteUser", async (req, res) => {
   console.log("DELETE API HIT");
   const request = req.body.firstName;
   try {
-    const deleteUser = await User.deleteOne({firstName: request});
-    if(deleteUser){
+    const deleteUser = await User.deleteOne({ firstName: request });
+    if (deleteUser) {
       const allUsers = await User.find({});
       res.send(allUsers);
-    };
+    }
   } catch (error) {
     res.status(401).send("Not Deleted");
-  };
+  }
 });
 
 server.patch("/updateUser", async (req, res) => {
   const userId = req.body.userId;
   const updateReq = req.body;
-  console.log(req.body);
-  console.log(req.body.userId);
-  try{
-    const user = await User.findByIdAndUpdate(userId, updateReq, {returnDocument: "after"});
+  try {
+    const user = await User.findByIdAndUpdate(userId, updateReq, {
+      returnDocument: "after",
+      runValidators: true,
+    });
     res.send(user);
-  } catch(error){
+  } catch (error) {
     res.status(400).send("Not Updated");
-  };
-})
+    console.log(error.message);
+  }
+});
 
 connectDB()
   .then(() => {
